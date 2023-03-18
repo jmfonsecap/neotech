3ac7a62b62559a6fef7b16a62113b65228bef2fa
Isaac <isaactadinagiraldo02@gmail.com>
2023-03-17 19:06:43 -0500

    simple login setted

--- neotechproject/routes/web.php
+++ neotechproject/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+
+Auth::routes();
+
+# Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
