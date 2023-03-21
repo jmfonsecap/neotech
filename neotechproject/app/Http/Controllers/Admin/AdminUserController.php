91e759f66197d51daf9aea5f827b6be5e7c6e6bd
“jmfonsecap” <jmfonsecap@eafit.edu.co>
2023-03-21 15:38:34 -0500

    Modificacion de hasMany parts y computer, mejora layout

--- neotechproject/app/Http/Controllers/Admin/AdminUserController.php
+++ neotechproject/app/Http/Controllers/Admin/AdminUserController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminUserController extends Controller
 {
     public function index()
     {
         $viewData = [];
-        $viewData["title"] = "Users dashboard";
-        $viewData["users"] = User::all();
-        return view('admin.users.index')->with("viewData", $viewData);
+        $viewData['title'] = 'Users dashboard';
+        $viewData['users'] = User::all();
+
+        return view('admin.users.index')->with('viewData', $viewData);
     }
 }
--- neotechproject/routes/web.php
+++ neotechproject/routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
-Route::get('/admin/users', 'App\Http\Controllers\Admin\AdminUserController@index')->name("admin.users.index");
+Route::get('/admin/users', 'App\Http\Controllers\Admin\AdminUserController@index')->name('admin.users.index');
 
 Route::get('/computers/create', 'App\Http\Controllers\ComputerController@create')->name('computer.create');
 Route::post('/computers/save', 'App\Http\Controllers\ComputerController@save')->name('computer.save');
