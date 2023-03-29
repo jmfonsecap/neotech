14bda0fb85e4e3573f0ad76b152b8a756fdea673
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2023-03-29 01:12:20 -0500

    Review Routes

--- neotechproject/routes/web.php
+++ neotechproject/routes/web.php
@@ -37,6 +37,13 @@ Route::get('/admin/parts/update/{id}', 'App\Http\Controllers\Admin\AdminPartCont
 Route::post('/admin/parts/{id}', 'App\Http\Controllers\Admin\AdminPartController@update')->name('admin.part.update');
 Route::get('/admin/parts/del/{id}', 'App\Http\Controllers\Admin\AdminPartController@delete')->name('admin.part.delete');
 
+Route::get('/admin/reviews', 'App\Http\Controllers\Admin\AdminReviewController@index')->name('admin.review.index');
+Route::get('/admin/review/{id}', 'App\Http\Controllers\Admin\AdminReviewController@show')->name('admin.review.show');
+Route::post('/admin/review/save', 'App\Http\Controllers\Admin\AdminReviewController@save')->name('admin.review.save');
+Route::get('/admin/review/update/{id}', 'App\Http\Controllers\Admin\AdminReviewController@edit')->name('admin.review.edit');
+Route::post('/admin/review/{id}', 'App\Http\Controllers\Admin\AdminReviewController@update')->name('admin.review.update');
+Route::get('/admin/review/del/{id}', 'App\Http\Controllers\Admin\AdminReviewController@delete')->name('admin.review.delete');
+
 Route::get('/admin/computers', 'App\Http\Controllers\Admin\AdminComputerController@index')->name('admin.computer.index');
 Route::get('/admin/computers/create', 'App\Http\Controllers\Admin\AdminComputerController@create')->name('admin.computer.create');
 Route::post('/admin/computers/save', 'App\Http\Controllers\Admin\AdminComputerController@save')->name('admin.computer.save');
